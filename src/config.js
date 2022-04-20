e996bf6d4125860865fcb1eb5c8403766d5b207b
Harish Raisinghani <harishraisinghani@users.noreply.github.com>
2022-04-20 04:52:53 +0000

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export const CONFIG = {
+  TEMPLATE: {
+    // XY = K Config
+    "key": "ckey_c219e0eef35648a89dffe25e770",
+
+    // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
+    "title": "XY=K DEX Dashboard",
+
+    // 4. (Optional) Set your banner image
+    "banner_picture": "",
+  },
+
+}
