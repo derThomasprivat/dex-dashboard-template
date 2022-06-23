f75f334c19a276d66b48dd4c2920e2ba23635ba8
harishcovalent <>
2022-06-23 20:17:42 +0000

    Update config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your Covalent API key (the one used here is a default for all of our templates)
-    "key": "ckey_6bf60a7bf22d4a309dbe74f3c5c",
+    "key": "ckey_3bdabb7325ee47cc9bcc1e5f3d7",
 
     // 2. Set the title of your DEX dashboard
     "title": "XY=K DEX Dashboard",
