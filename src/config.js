83b01c8fc61c48ecfe7986c409634be1ff4b22d9
derThomasprivat <derThomasprivat@users.noreply.github.com>
2022-12-14 05:47:15 +0100

    add api-key

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your Covalent API key (the one used here is a default for all of our templates)
-    "key": "ckey_3bdabb7325ee47cc9bcc1e5f3d7",
+    "key": "ckey_4515470f07124871b2fe44e8e29",
 
     // 2. Set the title of your DEX dashboard
-    "title": "XY=K DEX Dashboard",
+    "title": "dt(XY=K) DEX Dashboard",
 
     // 3. (Optional) Set  your header banner image
     "banner_picture": "",
